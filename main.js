48c5cd0506655e1961c73cfacac49940b8b2dbfd
Steven Schwengel <75495171+MustardTop@users.noreply.github.com>
2021-01-25 17:57:43 -0500

    ugly website content

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("hi");
+
+
+// document.getElementById("myBtn").addEventListener("click", function() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// });
+
+// var img = document.getElementById("myImage");
+var img = document.getElementById("bigButton");
+
+img.addEventListener("click", function(){
+  if(img.src != "truck1.jpg"){
+    img.src = "redtruck.jpg";}
+  else{
+    img.src = "redtruck.jpg";}});
